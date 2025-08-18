e554fa93670a53d2377b36e761cca6b05e0b19b1
RAVI TEJA KARANAM <eswarvanapalli9299@gmail.com>
2025-08-18 12:27:10 +0530

    added this code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import{BrowserRouter,Routes,Route} from "react-router-dom"
 import Python from "./python";
-import Home from "./home";
 import JavaScript from "./javascript";
+import Css from "./css";
 import HTML from "./html";
 import Sql from "./sql";
 import Signup from "./signup";
 import Login from "./login";
-import Css from "./css";
+import Home from "./home";
+
 
 function App(){
   return(
@@ -18,11 +19,11 @@ function App(){
       <Route path="/python" element={<Python/>}></Route>
       <Route path="/javascript" element={<JavaScript/>}></Route>
       <Route path="/Css" element={<Css/>}></Route>
-      <Route path="/HTML" element={<HTML/>}></Route>
-      <Route path="/Sql" element={<Sql/>}></Route>
+      <Route path="/HTML" element={< HTML/>}></Route>
+      <Route path="/Sql" element={< Sql/>}></Route>
       <Route path="/" element={<Signup/>}></Route>
       <Route path="/login" element={<Login/>}> </Route>
-      <Route path="/home" element={<Home/>}></Route>     
+      <Route path="/home" element={< Home/>}></Route>     
 
       </Routes>
       </BrowserRouter>
