f9c8064d43fcc6041485edfc77e92d1f62483813
RAVI TEJA KARANAM <eswarvanapalli9299@gmail.com>
2025-09-01 12:42:57 +0530

    updated

--- src/javascript.jsx
+++ src/javascript.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "./javaScript.css";
+import "./javascript.css";
 
 function JavaScript() {
     const navigate = useNavigate();
